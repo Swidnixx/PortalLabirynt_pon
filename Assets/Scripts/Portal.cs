e869d43824e908ff59114e1d3930bf0dd4a0c15e
Swidnixx <swidnixx@gmail.com>
2024-02-26 12:17:15 +0100

    Portal Mirror

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class Portal : MonoBehaviour
 {
     [SerializeField] Portal otherPortal;
-    [SerializeField] Material material;
+    UnityEngine.Camera myCamera;
+    Transform player;
 
-    public Camera myCamera;
+    public PortalTeleport tep { get; private set; }
 
-    public Transform renderSurface;
-    public Transform portalCollider;
+    public Renderer renderSurface;
 
-    
+    private void Awake()
+    {
+        tep = GetComponentInChildren<PortalTeleport>();
+    }
+    private void Start()
+    {
+        myCamera = GetComponentInChildren<UnityEngine.Camera>();
+        player = UnityEngine.Camera.main.transform;
 
+        RenderTexture rt = new RenderTexture(Screen.width, Screen.height, 0);
+        myCamera.targetTexture = rt;
+        otherPortal.renderSurface.material.SetTexture("_MainTex", rt);
+    }
+    private void Update()
+    {
+        Matrix4x4 m = transform.localToWorldMatrix *
+            otherPortal.transform.worldToLocalMatrix *
+            player.localToWorldMatrix;
 
+        myCamera.transform.SetPositionAndRotation( m.GetColumn(3), m.rotation);
+    }
 }
